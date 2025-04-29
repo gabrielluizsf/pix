3590bf13f2fa3040c0e2b50e49ca6206952ebd56
gabrielluizsf <93672097+gabrielluizsf@users.noreply.github.com>
2025-04-29 10:08:29 -0300

    feat: Implement PIX code reader
    This commit introduces a `Read` function that parses a copy-pasted PIX code and generates an `Options` struct.
    
    The `Read` function utilizes `buildUsingGuideMap` and `buildDataMap` to process the input string. It then extracts relevant data into an `Options` struct, handling a specific case where the TransactionID might be ",\*\*\*".

---
+++ reader.go
@@ -0,0 +1,34 @@
+package pix
+
+import "fmt"
+
+// Read generates a Options struct using a copyPaste PIX code
+func Read(copyPaste string) (Options, error) {
+	data := buildUsingGuideMap(copyPaste, buildDataMap(Options{}))
+	options, err := readDataMap(data)
+	return options, err
+}
+
+type intMap map[int]any
+
+func readDataMap(data intMap) (op Options, err error) {
+	keyMap, ok := data[26].(intMap)
+	if !ok {
+		return op, fmt.Errorf("data[26] is not (intMap)")
+	}
+	txMap := data[62].(intMap)
+	if txMap[5].(string) == "***" {
+		txMap[5] = ""
+	}
+
+	op = Options{
+		Key:           keyMap[1].(string),
+		Description:   keyMap[2].(string),
+		Amount:        data[54].(float64),
+		Name:          data[59].(string),
+		City:          data[60].(string),
+		TransactionID: txMap[5].(string),
+	}
+
+	return op, err
+}

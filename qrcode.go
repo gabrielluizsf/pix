4ab2bf9d32501643bbfbfd405a6f1e66d6eb29fe
gabrielluizsf <93672097+gabrielluizsf@users.noreply.github.com>
2025-04-30 15:45:19 -0300

    refactor: Simplify QRCode function by removing unnecessary conditional
    The `QRCode` function in `qrcode.go` was refactored to directly return the result of the `qrcode.Encode` function, removing the unnecessary conditional statement that set a default size. This change simplifies the code and improves readability without affecting functionality, as the underlying `qrcode.Encode` function likely handles default size logic internally.

--- qrcode.go
+++ qrcode.go
@@ -4,11 +4,5 @@ import "github.com/i9si-sistemas/qrcode"
 
 // QRCode returns a graphical representation of the Copy and Paste code in a QR Code form.
 func QRCode(options QRCodeOptions) ([]byte, error) {
-	if options.Size == 0 {
-		options.Size = 256
-	}
-
-	bytes, err := qrcode.Encode(options.Content, qrcode.Medium, options.Size)
-
-	return bytes, err
+	return qrcode.Encode(options.Content, qrcode.Medium, options.Size)
 }
